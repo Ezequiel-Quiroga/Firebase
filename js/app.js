53c86558da3c6c1e6462d677cc443a5b42c9db71
Ezequiel-Quiroga <eze@kirogait.com>
2017-03-15 01:05:36 -0300

    creando autenticacion
    creo lo que necesito para autenticarme en mi app

--- js/app.js
+++ js/app.js
@@ -14,7 +14,10 @@
   	//referencia al la bd
   	const dbRefObject=firebase.database().ref().child('objeto');
   	const dbRefList=dbRefObject.child('habilidades');
-
+	//sincronizo cambios de objeto
+  	dbRefObject.on('value',snap=> {
+  		preObject.innerText=JSON.stringify(snap.val(),null,3);
+  	});
   	//sincronizarlos cambios en la lista
   	dbRefList.on('child_added',snap=>{
   		const li=document.createElement('li');
@@ -23,19 +26,17 @@
   		ulList.appendChild(li);
   	});
 	//si se produce algun cambio lo detecta	
-  	dbRefList.on('child_added',snap=>{
-  		const liChanged=document.getElementById('snap.key');
+  	dbRefList.on('child_changed',snap=>{
+  		const liChanged=document.getElementById(snap.key);
   		liChanged.innerText=snap.val();
   	});
 
   	//si se produce la eliminacion de un objeto lo detecta	
-  	dbRefList.on('child_added',snap=>{
-  		const liRemoved=document.getElementById('snap.key');
+  	dbRefList.on('child_removed',snap=>{
+  		const liRemoved=document.getElementById(snap.key);
   		liRemoved.remove();
   	});
 
-  	//sincronizo cambios de objeto
-  	dbRefObject.on('value',snap=> {
-  		preObject.innerText=JSON.stringify(snap.val(),null,3);
-  	});
+  	
+
 } ());
